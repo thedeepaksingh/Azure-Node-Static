7e0405460f96d8de5b5108bcebddd322235e16a6
the_ds <128127664+thedeepaksingh@users.noreply.github.com>
2025-05-21 01:10:56 +0530

    changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Import routes
 import indexRouter from "./routes/indexroutes.js";
